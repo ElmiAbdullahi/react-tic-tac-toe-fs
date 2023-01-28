2be07f5d5bafe9e56f9474fba34f91f42c5d7b15
Abdullahi Elmi <abdullahielmi147@gmail.com>
2023-01-27 17:55:45 -0800

    Created Board component which reads the board state from the context and render a box component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useContext } from 'react';
+import { GameContext } from '../../services/GameContext.js';
+import Box from '../Box/Box.js';
 
 export default function Board() {
-  return <div>Board</div>;
+  const { gameBoard } = useContext(GameContext);
+  return (
+    <div>
+      {gameBoard.map(({ space, content }) => {
+        <Box key={space} space={space} content={content} />;
+      })}
+    </div>
+  );
 }
